0dfd07fe26fdfdcb4ef84816a3584de5fe5a4790
haris96 <haris@fit-outfit.com>
2022-05-24 12:53:47 +0500

    working with sliding window

--- [0-algorithms]/25-subset-algorithms.js
+++ [0-algorithms]/25-subset-algorithms.js
@@ -81,3 +81,28 @@ function getImmediateSubsets(arr) {
 
 // getImmediateSubsets([1, 2, 3, 4, 5])
 console.log(getImmediateSubsets([1, 2, 3, 4, 5]))
+// *************************************************************************************
+function getImmediateSubsets(arr) {
+  const allSubsets = []
+  for (let i = 0; i < arr.length; i++) {
+    let subsets = []
+    subsets.push([arr[i]])
+
+    for (let j = i + 1; j < arr.length; j++) {
+      // subsets.push([...subsets[j - (i + 1)], arr[j]])
+      subsets.push(subsets[j - (i + 1)].concat(arr[j]))
+    }
+
+    allSubsets.push(subsets)
+  }
+
+  return allSubsets
+}
+
+// getImmediateSubsets([1, 2, 3, 4, 5])
+console.log(getImmediateSubsets([1, 2, 3, 4, 5]))
+
+// *************************************************************************************
+// *************************************************************************************
+// *************************************************************************************
+// *************************************************************************************
--- [0-algorithms]/test.js
+++ [0-algorithms]/test.js
@@ -1,16 +1,13 @@
-function getImmediateSubsets(arr) {
+function getImmediateSubsets(arr, num) {
   const allSubsets = []
-
   for (let i = 0; i < arr.length; i++) {
     let subsets = []
+    subsets.push([arr[i]])
 
-    for (let j = i; j < arr.length; j++) {
-      if (j !== i) {
-        subsets.push([...subsets[j - (i + 1)], arr[j]])
-      } else {
-        subsets.push([arr[j]])
-      }
+    for (let j = i + 1; j < arr.length; j++) {
+      subsets.push(subsets[j - (i + 1)].concat(arr[j]))
     }
+
     allSubsets.push(subsets)
   }
 
@@ -18,4 +15,4 @@ function getImmediateSubsets(arr) {
 }
 
 // getImmediateSubsets([1, 2, 3, 4, 5])
-console.log(getImmediateSubsets([1, 2, 3, 4, 5]))
+console.log(getImmediateSubsets([1, 2, 3, 4, 5], 1))
