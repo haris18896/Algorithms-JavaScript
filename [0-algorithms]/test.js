171bd1044fc48398fbeec26effd7fb8cdde1bb06
haris96 <haris@fit-outfit.com>
2022-05-25 18:36:39 +0500

    prime code

--- [0-algorithms]/test.js
+++ [0-algorithms]/test.js
@@ -1,10 +1,16 @@
-function longestString(str) {
-  let longest = ''
-  let tempStr = ''
-  for (let i = 0; i < str.length; i++) {
-    
+function prime() {
+  for (let num = 2; num <= 150; num++) {
+    let i = 2
+    while (i <= num / 2) {
+      if (num % i === 0) {
+        break
+      }
+      i++
+    }
+    if (i > num / 2) {
+      console.log('Prime', num)
+    }
   }
-  return console.log('longest string: ', longest)
 }
 
-longestString('abbaabbbabbab')
+prime()
