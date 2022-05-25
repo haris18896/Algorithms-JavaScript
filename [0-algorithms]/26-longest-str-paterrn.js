955a45054dfbdb0f74ef3275e948e39dc182306a
haris96 <haris@fit-outfit.com>
2022-05-25 19:00:17 +0500

    adding file

---
+++ [0-algorithms]/26-longest-str-paterrn.js
@@ -0,0 +1,20 @@
+function prime() {
+  let prime = []
+  for (let num = 2; num <= 150; num++) {
+    let i = 2
+    while (i <= num / 2) {
+      if (num % i === 0) {
+        break
+      }
+      i++
+    }
+    if (i > num / 2) {
+      prime.push(num)
+    }
+  }
+  console.log(prime)
+}
+
+prime()
+
+// *************************************************************************************
