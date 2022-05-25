e018e7726d229154ebb2700abd0d1e4dfb36e3c7
haris96 <haris@fit-outfit.com>
2022-05-25 12:12:51 +0500

    subsets greater then 5 has been solved

--- [0-algorithms]/25-subset-algorithms.js
+++ [0-algorithms]/25-subset-algorithms.js
@@ -141,4 +141,27 @@ function getImmediateSubsets(arr) {
 
 getImmediateSubsets([1, 2, 3, 4, 5])
 // *************************************************************************************
+function getImmediateSubsets(arr) {
+  const allSubsets = []
+  for (let i = 0; i < arr.length; i++) {
+    let subsets = []
+    subsets.push([arr[i]])
+    for (let j = i; j < arr.length; j++) {
+      if (j != i) {
+        subsets.push(subsets[j - (i + 1)].concat(arr[j]))
+        for (let k = 0; k < subsets.length; k++) {
+          let sum = subsets[k].reduce((a, b) => a + b, 0)
+          if (sum > 5) {
+            allSubsets.push(subsets[k])
+          }
+        }
+      }
+    }
+  }
+
+  return console.log('allSubsets : ', [...new Set(allSubsets)])
+}
+
+getImmediateSubsets([1, 2, 3, 4, 5])
+
 // *************************************************************************************
--- [0-algorithms]/test.js
+++ [0-algorithms]/test.js
@@ -3,15 +3,20 @@ function getImmediateSubsets(arr) {
   for (let i = 0; i < arr.length; i++) {
     let subsets = []
     subsets.push([arr[i]])
-
-    for (let j = i + 1; j < arr.length; j++) {
-      subsets.push(subsets[j - (i + 1)].concat(arr[j]))
+    for (let j = i; j < arr.length; j++) {
+      if (j != i) {
+        subsets.push(subsets[j - (i + 1)].concat(arr[j]))
+        for (let k = 0; k < subsets.length; k++) {
+          let sum = subsets[k].reduce((a, b) => a + b, 0)
+          if (sum > 5) {
+            allSubsets.push(subsets[k])
+          }
+        }
+      }
     }
-
-    allSubsets.push(subsets)
   }
 
-  return console.log('allSubsets : ', allSubsets)
+  return console.log('allSubsets : ', [...new Set(allSubsets)])
 }
 
 getImmediateSubsets([1, 2, 3, 4, 5])
