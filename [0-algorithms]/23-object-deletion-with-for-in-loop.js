a9d23a5ff6b2d8a6c8617183dbf50e648a70c3ae
haris96 <haris@fit-outfit.com>
2022-05-25 16:43:28 +0500

    find greater number of substring in a string

--- [0-algorithms]/23-object-deletion-with-for-in-loop.js
+++ [0-algorithms]/23-object-deletion-with-for-in-loop.js
@@ -22,3 +22,64 @@ console.log(
     f: 'N/A'
   })
 )
+
+// *************************************************************************************
+// solving recursively
+function objPractice(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (obj[key] === 'N/A' || obj[key] === '' || obj[key] === '-') {
+        delete obj[key]
+      } else if (Array.isArray(obj[key])) {
+        obj[key] = obj[key].filter(item => item != '' && item != 'N/A' && item != '-')
+      }
+      if (typeof obj[key] === 'object') {
+        objPractice(obj[key])
+      }
+    }
+  }
+  return obj
+}
+
+console.log(
+  objPractice({
+    a: 'N/A',
+    b: '',
+    c: 12,
+    d: 'abc',
+    t: '-',
+    e: {
+      ea: 12,
+      eb: '-',
+      et: ['N/A', '', '-', 32, 'avs'],
+      ec: 456,
+      ed: 'N/A',
+      ee: {
+        eea: 124,
+        eeb: '',
+        eec: 098,
+        eed: 'N/A'
+      }
+    },
+    f: 'N/A',
+    g: '-',
+    h: {
+      ha: 'N/A',
+      hb: '',
+      hc: '-',
+      hd: '789',
+      he: 'ads',
+      hf: {
+        hfa: 'N/A',
+        hfb: '',
+        hfc: '-',
+        hfd: '123',
+        hfe: 'N/A',
+        hff: 'oop'
+      },
+      hg: ['N/A', '', '-', 32, 'avs']
+    }
+  })
+)
+
+// *************************************************************************************
--- [0-algorithms]/test.js
+++ [0-algorithms]/test.js
@@ -1,21 +1,12 @@
-function getImmediateSubsets(arr, num) {
-  const allSubsets = []
-  for (let i = 0; i < arr.length; i++) {
-    let subsets = []
-    let sum = 0
-    subsets.push([arr[i]])
-    for (let j = i; j < arr.length; j++) {
-      if (j != i) {
-        subsets.push(subsets[j - (i + 1)].concat(arr[j]))
-      }
-      sum = subsets[j - i].reduce((a, b) => a + b, 0)
-      if (sum < num) {
-        allSubsets.push(subsets[j - i])
-      }
+function longestString(str) {
+  let longest = ''
+  for (let i = 0; i < str.length; i++) {
+    let tempStr = ''
+    for (let j = i; j < str.length; j++) {
+      console.log('i : ', str[i], 'j : ', str[j])
     }
   }
-
-  return console.log('allSubsets : ', [...new Set(allSubsets)])
+  return console.log('longest string: ', longest)
 }
 
-getImmediateSubsets([1, 2, 3, 4, 5], 5)
+longestString('abbaabbbabbab')
