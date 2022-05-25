ebe5a13625f391731cd2bf05f741d72e82de9441
haris96 <haris@fit-outfit.com>
2022-05-25 16:50:00 +0500

    working with substrings

--- [0-algorithms]/test.js
+++ [0-algorithms]/test.js
@@ -1,10 +1,8 @@
 function longestString(str) {
   let longest = ''
+  let tempStr = ''
   for (let i = 0; i < str.length; i++) {
-    let tempStr = ''
-    for (let j = i; j < str.length; j++) {
-      console.log('i : ', str[i], 'j : ', str[j])
-    }
+    
   }
   return console.log('longest string: ', longest)
 }
